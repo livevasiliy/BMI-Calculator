878f614577235c5d3401281f6aa2859898aa59b3
Vasiliy Pivovarov <pivovarov.v.s@appfox.ru>
2022-07-26 01:39:44 +0400

    Finish 11 module

---
+++ BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Василий Пивоваров on 25.07.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,37 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Василий Пивоваров on 25.07.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain {
+    
+    var bmi: BMI?
+    
+    mutating func calculateBMI(height: Float, weight: Float) -> Void {
+        let bmiValue = weight / (height * height)
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1))
+        }
+    }
+    
+    func getBMIValue() -> String {
+        return String(format: "%.1f", bmi?.value ?? 0.0)
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .white
+    }
+}
